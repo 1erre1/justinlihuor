1045c08041167f6b2bc1f4ec856180ef2488f0a4
1erre1 <lihuor.lly@gmail.com>
2026-07-15 14:09:06 +0700

    hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,120 +1,98 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import logo from "./assets/brack.png";
+// import background from "./assets/back.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([
+
+  ]);
+
+  const [newMovieTitle, setNewMovieTitle] = useState('')
+
+  function addMovie(event) {
+    event.preventDefault();
+
+    if (newMovieTitle.trim() === '') {
+      return;
+    }
+
+    const newMovie = {
+      id: Date.now(),
+      title: newMovieTitle,
+      isCompleted: false
+    };
+
+    setMovies([...movies, newMovie]);
+    setNewMovieTitle('');
+  }
+
+  function completeMovie(movieId) {
+    const updatedMovies = movies.map((movie) => {
+      if (movie.id === movieId) {
+        return { ...movie, isCompleted: true }
+      }
+
+      return movie;
+    })
+    setMovies(updatedMovies)
+  }
+
+  function clearMovie() {
+    setMovies([]);
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      {/* <div
+        className="app"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}> */}
+      <main className='main'>
+        <div className='box'>
+          <img src={logo} alt="Logo" />
+          <div className='header-text'>
+            <h1>Name</h1>
+            <p>Catchy line</p>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+        <div className='box'>
+          <h4>Total movies: {movies.length}</h4>
+          <form onSubmit={addMovie}>
+            <input
+              value={newMovieTitle}
+              onChange={(event) => setNewMovieTitle(event.target.value)}
+              placeholder='Add new movie'
+            />
+            <button type='submit'>Add Movie</button>
+            <button type='button' onClick={clearMovie}>
+              Clear All
+            </button>
+          </form>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          <section className='movie-list'>
+            {movies.length === 0 ? (
+              <p className='empty'>No movies yet.</p>
+            ) :
+              movies.map((movie) => (
+                <article className='movie-card' key={movie.id}>
+                  <div>
+                    <h2>{movie.title}</h2>
+                    <p>Status: {movie.isCompleted ? 'Completed' : 'Not yet'}</p>
+                  </div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                  <button onClick={() => completeMovie(movie.id)}>
+                    {movie.isCompleted ? "Done" : "Mark as completed"}
+                  </button>
+
+                </article>
+              ))}
+          </section>
+        </div>
+      </main>
+      {/* </div> */}
     </>
   )
 }
